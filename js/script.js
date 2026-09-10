d70b5d1d1d04d90d6a1a97787fe79b6fd65c2353
amandhanafilaa <amandhanafilaa@gmail.com>
2026-09-10 20:53:05 +0700

    feat: tambah informasi lokasi UMKM

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 const promoButton = document.querySelector("#promoButton");
+const pesanButton = document.getElementById("pesanButton");
 
 promoButton.addEventListener("click", () => {
   promoButton.textContent = "Promo: Beli 2 gratis 1!";
   console.log("Promo Zeze Collection berhasil ditampilkan.");
+});
+
+pesanButton.addEventListener("click", () => {
+  pesanButton.textContent = "Pemesanan sedang diproses...";
+  console.log("Pemesanan berhasil dikirim.");
 });
